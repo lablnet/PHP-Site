21240b0b97e6d3b6f7c8a1d396509e459cf8a55b
Malik Umer Farooq <Lablnet01@gmail.com>
2018-07-26 06:32:45 +0000

    Apply fixes from StyleCI

--- classes/Site.php
+++ classes/Site.php
@@ -1,99 +1,83 @@
 <?php
-	/**
-	 * PHP Site class
-	 *
-	 * @author   Malik Umer Farooq <lablnet01@gmail.com>
-	 * @author-profile https://www.facebook.com/malikumerfarooq01/
-	 * @license MIT 
-	 * @link    https://github.com/Lablnet/PHP-Site-class
-	 *
-	 */
+/**
+ * PHP Site class.
+ *
+ * @author   Malik Umer Farooq <lablnet01@gmail.com>
+ * @author-profile https://www.facebook.com/malikumerfarooq01/
+ *
+ * @license MIT
+ *
+ * @link    https://github.com/Lablnet/PHP-Site-class
+ */
 class Site
-
 {
+    /**
+     * Return site URL.
+     *
+     * @return string
+     */
+    public static function SiteUrl()
+    {
+        $base_url = self::GetProtocol().self::GetServerName().':'.self::GetPort().self::GetUri();
 
-		/**
-		 * Return site URL
-		 * 
-		 * @access public
-		 * @return string
-		 */	
-	public static function SiteUrl(){
+        return $base_url;
+    }
 
-		    $base_url = Site::GetProtocol() . Site::GetServerName() . ':' . Site::GetPort() . Site::GetUri();
+    /**
+     * Return Current Page.
+     *
+     * @return string
+     */
+    public static function CurrentPage()
+    {
+        $base_url = self::GetUri();
 
-			return $base_url;
+        return $base_url;
+    }
 
-	}
-		/**
-		 * Return Current Page
-		 * 
-		 * @access public
-		 * @return string
-		 */		
-	public static function CurrentPage(){
-
-		    $base_url = Site::GetUri();
-
-			return $base_url;
-
-	}	
     /**
      * Get the domain protocol.
      *
-     * @access public    
      * @return string
-     */	
-	public static function GetProtocol(){
+     */
+    public static function GetProtocol()
+    {
+        if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
+            $protocol = 'http://';
+        } else {
+            $protocol = 'https://';
+        }
 
-		if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
-
-			$protocol = 'http://';
-
-		} else {
-
-			$protocol = 'https://';
-
-		}		
-
-		return $protocol;
-			
-	}	
-
+        return $protocol;
+    }
 
     /**
      * Get the server name.
      *
-     * @access public    
      * @return string
      */
-     public static function GetServerName(){
+    public static function GetServerName()
+    {
+        if (isset($_SERVER['SERVER_NAME'])) {
+            return $_SERVER['SERVER_NAME'];
+        } else {
+            return false;
+        }
+    }
 
-     	if(isset($_SERVER['SERVER_NAME'])){
-
-     		return $_SERVER['SERVER_NAME'];
-
-     	}else{
-
-     		return false;
-
-     	}
-
-     }	
     /**
      * Get the server port.
      *
-     * @access public
      * @return int
-     */	
+     */
     public static function GetPort()
     {
         return $_SERVER['SERVER_PORT'];
     }
+
     /**
      * Get script path like example.com/login.
      *
-     * @access public
      * @return string example.com/login
      */
     public static function GetUri()
@@ -101,87 +85,70 @@ class Site
         return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 
-	/**
-	 * Redirect to another page
-	 *
-	 * @param (string) $url optional 
-	 * self => itself page
-	 * else => any page you want
-	 * 
-     * @access public
-	 * @return void
-	 */
-	public static function Redirect( $url = null ){
+    /**
+     * Redirect to another page.
+     *
+     * @param (string) $url optional
+     *                      self => itself page
+     *                      else => any page you want
+     *
+     * @return void
+     */
+    public static function Redirect($url = null)
+    {
+        if ($url === null or empty($url)) {
+            $base_url = self::SiteUrl();
+        } elseif ($url === 'self' or isset($_SERVER['HTTP_REFERER'])) {
+            $base_url = self::previous();
+        } elseif ($url !== 'self' && $url !== null) {
+            $base_url = self::SiteUrl().$url;
+        } else {
+            $base_url = $url;
+        }
 
-		if($url === null or empty($url)){
+        header('Location:'.$base_url);
+    }
 
-			$base_url = self::SiteUrl();
-
-		}elseif($url === 'self' or isset($_SERVER['HTTP_REFERER'])) {
-
-	        $base_url = Site::previous();
-
-	    }elseif($url !== 'self' && $url !== null){
-
-	    	$base_url = Site::SiteUrl().$url;
-
-	    }else{
-
-			$base_url = $url;
-
-		}
-
-		header("Location:".$base_url);
-
-	}
-	/**
-	* Go to the previous URL.
-	* @access private
-	* @return void
-	*/	
+    /**
+     * Go to the previous URL.
+     *
+     * @return void
+     */
     private static function Previous()
     {
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
+        header('Location: '.$_SERVER['HTTP_REFERER']);
         exit;
-    }	
+    }
+
     /**
      * Get all URL parts based on a / seperator.
      *
      * @param string $url → URI to segment
      *
-     * @access public
      * @return string
      */
     public static function SegmentUrl($url = null)
     {
-       	if(!is_null($url) && !empty($url)){
-
-       		$url = $url;
-
-       	}else{
-
-       		$url = $_SERVER['REQUEST_URI'];
-
-       	}
+        if (!is_null($url) && !empty($url)) {
+            $url = $url;
+        } else {
+            $url = $_SERVER['REQUEST_URI'];
+        }
 
         return explode('/', trim($url, '/'));
-    }  
+    }
+
     /**
      * Get first item segment.
      *
-     * @access public     
      * @return string
      */
     public static function GetFirstSegment($segments)
     {
-        if(is_array($segments)){
-
-        	$vars = $segments;
-
-        }else{
-
-        	$vars = Site::SegmentUrl($segments);
-
+        if (is_array($segments)) {
+            $vars = $segments;
+        } else {
+            $vars = self::SegmentUrl($segments);
         }
 
         return current($vars);
@@ -191,46 +158,37 @@ class Site
      * Get last item segment.
      *
      * @return string
-     * @access public    
      */
     public static function GetLastSegment($segments)
     {
-        if(is_array($segments)){
-
-        	$vars = $segments;
-
-        }else{
-
-        	$vars = Site::SegmentUrl($segments);
-
+        if (is_array($segments)) {
+            $vars = $segments;
+        } else {
+            $vars = self::SegmentUrl($segments);
         }
 
         return end($vars);
-    }      	
+    }
 
-	/**
-	* generate salts for files
-	* 
-	* @param string $length length of salts
-    * @access public	
-	* @return string
-	*/
-	public static function Salts($length){
-		
-		$chars =  array_merge(range(0,9), range('a', 'z'),range('A', 'Z'));
+    /**
+     * generate salts for files.
+     *
+     * @param string $length length of salts
+     *
+     * @return string
+     */
+    public static function Salts($length)
+    {
+        $chars = array_merge(range(0, 9), range('a', 'z'), range('A', 'Z'));
 
-		$stringlength = count( $chars  ); //Used Count because its array now
-		
-		$randomString = '';
-		
-		for ( $i = 0; $i < $length; $i++ ) {
-			
-			$randomString .= $chars[rand( 0, $stringlength - 1 )];
-			
-		}
-		
-		return $randomString;
-		
-	}
+        $stringlength = count($chars); //Used Count because its array now
 
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $chars[rand(0, $stringlength - 1)];
+        }
+
+        return $randomString;
+    }
 }
--- index.php
+++ index.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
+
 
 require_once 'init.php';
 
-echo "site url: ";
+echo 'site url: ';
 //for site url
 echo Site::SiteUrl();
 
-echo "<br> Random salts: ";
+echo '<br> Random salts: ';
 
 //for slug,random string
 echo Site::Salts(10);
 
 //for redirect
 //Site::Redirect();
-
